a1b757c1e7b5f171b99ed3a8e302b4fd6f3b232f
eymen burak <eymenburak@hotmail.com>
2021-01-01 22:08:08 +0300

    Add project files.

---
+++ SignalRCore.API/Hubs/AppHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRCore.API.Hubs
+{
+    public class AppHub:Hub
+    {
+        public static List<string> Names { get; set; } = new List<string>();
+        public async Task SendMessage(string name)
+        {
+            Names.Add(name);
+           await  Clients.All.SendAsync("ReceiveName", name);
+        }
+
+        public async Task GetNames()
+        {
+            await Clients.All.SendAsync("ReceiveNames", Names);
+        }
+    }
+}
