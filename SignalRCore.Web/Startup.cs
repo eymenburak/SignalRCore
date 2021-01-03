465cab4586b25020b0eda5adb11d88871d587c00
eymen burak <eymenburak@hotmail.com>
2021-01-03 22:32:04 +0300

    SignalR library was updated

--- SignalRCore.Web/Startup.cs
+++ SignalRCore.Web/Startup.cs
@@ -1,18 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using SignalRCore.API.Hubs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SignalRCore.API
+namespace SignalRCore.Web
 {
     public class Startup
     {
@@ -26,16 +23,7 @@ namespace SignalRCore.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options => 
-            {
-                options.AddPolicy("CorsPolicy", builder => 
-                {
-                    builder.WithOrigins("https://localhost:44375").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
-                });
-            
-            });
-            services.AddControllers();
-            services.AddSignalR();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,17 +33,24 @@ namespace SignalRCore.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
-            app.UseCors("CorsPolicy");
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
-                endpoints.MapHub<AppHub>("/AppHub");
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
