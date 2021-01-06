9591c4b8378255d09df74e1a9657a5c740f4972f
eymen burak <eymenburak@hotmail.com>
2021-01-06 20:26:03 +0300

    NıotificationController was added initially

---
+++ SignalRCore.API/Controllers/NotificationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRCore.API.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRCore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IHubContext<AppHub> _hubContext;
+
+        public NotificationController(IHubContext<AppHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpGet("{teamCount}")]
+        public async Task<IActionResult> SetTeamCount(int teamCount)
+        {
+            await _hubContext.Clients.All.SendAsync("Notify", $"All of team {teamCount}");
+
+            return Ok();
+        }
+    }
+}
