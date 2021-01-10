00db840f8b091c46307fc43cada1a18f48ff27de
eymen burak <eymenburak@hotmail.com>
2021-01-10 16:23:22 +0300

    Strong types, Groups, CodeFirstMigration were added.

--- SignalRCore.API/Startup.cs
+++ SignalRCore.API/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SignalRCore.API.Hubs;
+using SignalRCore.API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,11 @@ namespace SignalRCore.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConStr"]);
+            });
+
             services.AddCors(options => 
             {
                 options.AddPolicy("CorsPolicy", builder => 
